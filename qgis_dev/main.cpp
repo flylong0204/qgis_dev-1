1d23ffae2803d5513bdefd9faacf54c5b4bbcda7
nodnaba <1119196803@qq.com>
2017-12-31 09:46:25 +0800

    QSettings

--- qgis_dev/main.cpp
+++ qgis_dev/main.cpp
@@ -5,6 +5,11 @@
 
 int main(int argc, char *argv[])
 {
+	// Qsetting
+	QCoreApplication::setOrganizationName( "name" );
+	//QCoreApplication::setOrganizationDomain( "jacory.com" ); // 域名好像是可以不用加的
+	QCoreApplication::setApplicationName( "QGisDev" );
+
 	//QApplication a(argc, argv);
 	QgsApplication myApp(argc, argv, true);
 	QgsApplication::setPrefixPath("D:/Program Files (x86)/QGIS 2.18", true);
